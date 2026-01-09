65dbaefb2edb36019b8b58bf2916779ba12497d9
monteiro603 <abraoneivaldo5@gmail.com>
2026-01-09 18:18:47 -0300

    Add files via upload
    Versão inicial Legacy

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'lingoquest-legacy-v1';
+const ASSETS = [
+  './lingoquest_global_pro.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
